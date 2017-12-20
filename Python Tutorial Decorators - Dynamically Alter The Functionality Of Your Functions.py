e46e0325ed38ea13f3c47182e7f01e1dc8be34da
jpmolden <john.paul.molden@gmail.com>
2017-12-19 20:45:23 -0800

    More Examples with Decorators
    More Examples with Decorators

--- Python Tutorial Decorators - Dynamically Alter The Functionality Of Your Functions.py
+++ Python Tutorial Decorators - Dynamically Alter The Functionality Of Your Functions.py
@@ -13,29 +13,82 @@
 #     print("Exe inner")
 #     return inner_function()
 #
-# outer_function()
+# outer_function()Closures
 
 # Closures the inner function still has access to the variables in the local scope in which it was created.
 # Basically if return inner_function is used, my_func = outer_function() will "remember" the local variable message
 
 
-def outer_function(msg):
-    """
-    Example 1 - Using the local variable message
-    Inner function has access to the message variable
-    See the closures video https://www.youtube.com/watch?v=swU3c34d2NQ&feature=youtu.be
-    """
-    message = msg
+# # Closures
+# def outer_function(msg):
+#     """
+#     Example 1 - Using the local variable message
+#     Inner function has access to the message variable
+#     See the closures video https://www.youtube.com/watch?v=swU3c34d2NQ&feature=youtu.be
+#     """
+#     message = msg
+#
+#     def inner_function():
+#         """
+#         Prints out specific message
+#         """
+#         print(message)
+#
+#     print("Exeinner")
+#     return inner_function
+#
+# # my_func = outer_function()
+# # my_func()
+# # my_func()
+# # my_func()
+# hi_func = outer_function('Hi')
+# bye_func = outer_function('Bye')
+# hi_func()
 
-    def inner_function():
-        print(message)
+# # Decorator Example - Allows to easily add functionality to existing functions without modifying the original function
+# def decorator_function(original_function):
+#     def wrapper_function():
+#         print('Wrapper executed this before {}'.format(original_function.__name__))
+#         return original_function()
+#
+#     return wrapper_function
+#
+# def display():
+#     print('Display function ran')
+#
+# decorated_display = decorator_function(display)
+#
+# decorated_display()
 
-    print("Exeinner")
-    return inner_function
+# # Decorator Example - Alternate and typical syntax
+# def decorator_function(original_function):
+#     def wrapper_function():
+#         print('Wrapper executed this before {}'.format(original_function.__name__))
+#         return original_function()
+#
+#     return wrapper_function
+#
+# @decorator_function
+# def display():
+#     print('Display function ran')
+#
+# display()
 
-# my_func = outer_function()
-# my_func()
-# my_func()
-# my_func()
-hi_func = outer_function('Hi')
-bye_func = outer_function('Bye')
+
+# Decorator Example 3 - Alternate and typical syntax
+def decorator_function(original_function):
+    def wrapper_function(*args, **kwargs):
+        print('Wrapper executed this before {}'.format(original_function.__name__))
+        return original_function()
+
+    return wrapper_function
+
+@decorator_function
+def display():
+    print('Display function ran')
+
+def display_info(name, age):
+    print('Display info ran with arguments ({}, {})'.format(name, age))
+
+display()
+display_info('Bob', 50)
